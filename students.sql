19cd15cfb53de706a75df78e31e049d28bdd7300
yuito <yuito2224@gmail.com>
2022-09-15 17:44:40 +0900

    main

---
+++ students.sql
@@ -0,0 +1,9 @@
+create table student (
+    id int NOT NULL PRIMARY KEY, 
+    name varchar(30) NOT NULL ,
+    name_sub varchar(30) NOT NULL,
+    class varchar(3) NOT NULL,
+    grade int(3) NOT NULL,
+    department varchar(30) NOT NULL,
+    major varchar(30) NOT NULL
+);
