93d2d70b71ed8583d31eb4269e79a6cda3f83cb1
a <yuito@gmail>
2022-11-18 14:31:46 +0900

    main

--- sqls/insert_student.sql
+++ sqls/insert_student.sql
@@ -204,7 +204,60 @@ values(
     1,
     1,
     5,
-    2,
+    9,
     3,
     2
 );
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004240012',
+    '中村太一',
+    'ナカムラタイチ',
+    '男性',
+    20,
+    1,
+    1,
+    5,
+    10,
+    3,
+    2
+);
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004240012',
+    '中村太一',
+    'ナカムラタイチ',
+    '男性',
+    20,
+    1,
+    1,
+    5,
+    11,
+    3,
+    2
+);
+### こがと俺のじゃヴぁ中級追加
