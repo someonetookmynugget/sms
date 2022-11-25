e207d31ab25262919ff5ef403c11b4145efa5b31
yuito <yuito2224@>
2022-11-25 11:41:27 +0900

    main

--- sqls/insert_student.sql
+++ sqls/insert_student.sql
@@ -260,4 +260,264 @@ values(
     3,
     2
 );
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004230011',
+    '西結都',
+    'ニシユイト',
+    '男性',
+    21,
+    1,
+    2,
+    7,
+    6,
+    3,
+    2
+);
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004230011',
+    '西結都',
+    'ニシユイト',
+    '男性',
+    21,
+    1,
+    2,
+    7,
+    7,
+    3,
+    2
+);
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004230011',
+    '西結都',
+    'ニシユイト',
+    '男性',
+    21,
+    1,
+    2,
+    7,
+    8,
+    3,
+    2
+);
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004230006',
+    '古賀慶次郎',
+    'コガケイジロウ',
+    '男性',
+    21,
+    1,
+    2,
+    7,
+    6,
+    3,
+    2
+);
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004230006',
+    '古賀慶次郎',
+    'コガケイジロウ',
+    '男性',
+    21,
+    1,
+    2,
+    7,
+    7,
+    3,
+    2
+);
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004230006',
+    '古賀慶次郎',
+    'コガケイジロウ',
+    '男性',
+    21,
+    1,
+    2,
+    7,
+    8,
+    3,
+    2
+);
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004230006',
+    '古賀慶次郎',
+    'コガケイジロウ',
+    '男性',
+    21,
+    1,
+    2,
+    7,
+    12,
+    3,
+    2
+);
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004230006',
+    '古賀慶次郎',
+    'コガケイジロウ',
+    '男性',
+    21,
+    1,
+    2,
+    7,
+    13,
+    3,
+    2
+);
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004230011',
+    '西結都',
+    'ニシユイト',
+    '男性',
+    21,
+    1,
+    2,
+    7,
+    13,
+    3,
+    2
+);
+insert into student (
+    student_id,
+    name,
+    name_sub,
+    gender,
+    age,
+    department_id,
+    major_id,
+    class_id,
+    subject_id,
+    grade,
+    total_unit
+    )
+values(
+    '2004230011',
+    '西結都',
+    'ニシユイト',
+    '男性',
+    21,
+    1,
+    2,
+    7,
+    12,
+    3,
+    2
+);
 ### こがと俺のじゃヴぁ中級追加
